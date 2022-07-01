df50ed0fedce4e866348f103fe8340a9f57e983d
Matthew Wells <56666479+TheGuy987@users.noreply.github.com>
2022-06-30 20:30:36 -0600

    Working test
    Increased stack size of threads to make the included test work.

--- RTOSdev/OSv1/Core/Src/threads.c
+++ RTOSdev/OSv1/Core/Src/threads.c
@@ -20,13 +20,13 @@ extern struct threadNode* head;
 //PCD_HandleTypeDef hpcd_USB_OTG_FS;
 
 void mainThread(void *argument){
-	startThread(test1, osPriorityLow, "One", 4*64);
+	startThread(test1, osPriorityLow, "One", 8*64);
 	for(;;){
 		//vTaskDelay(pdMS_TO_TICKS(1000));
-		sendUartMessage("zro", 3);
-		printAllThreadInfo();
-		sendUartMessage("\n", 1);
-		sendUartMessage("\r", 1);
+//		sendUartMessage("zro", 3);
+//		printAllThreadInfo();
+//		sendUartMessage("\n", 1);
+//		sendUartMessage("\r", 1);
 		vTaskDelay(pdMS_TO_TICKS(50));
 //		killThread(findNode(osThreadGetId())->children[0]);
 //		if(mainProcessCount != childCount(head)){
@@ -38,7 +38,7 @@ void mainThread(void *argument){
 //Test function
 void test1(void *argument)
 {
-  startThread(test2, osPriorityLow, "Two", 4*64);
+  startThread(test2, osPriorityLow, "Two", 8*64);
   //vTaskDelay(pdMS_TO_TICKS(2000));
   sendUartMessage("Uno", 3);
   printAllThreadInfo();
@@ -46,12 +46,12 @@ void test1(void *argument)
   sendUartMessage("\r", 1);
   for(;;)
   {
-	sendUartMessage("\n", 1);
-	sendUartMessage("\r", 1);
-	sendUartMessage("111111", 6);
-	sendUartMessage("\n", 1);
-	sendUartMessage("\r", 1);
-    vTaskDelay(pdMS_TO_TICKS(50));
+//	sendUartMessage("\n", 1);
+//	sendUartMessage("\r", 1);
+//	sendUartMessage("111111", 6);
+//	sendUartMessage("\n", 1);
+//	sendUartMessage("\r", 1);
+    vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
@@ -59,24 +59,24 @@ void test1(void *argument)
 void test2(void *argument)
 {
 	//startThread(test3, osPriorityHigh, "fre", 4*64);
-	//vTaskDelay(pdMS_TO_TICKS(3000));
+	vTaskDelay(pdMS_TO_TICKS(500));
 	sendUartMessage("duo", 3);
 	printAllThreadInfo();
-	sendUartMessage("\n", 1);
-	sendUartMessage("\r", 1);
+//	sendUartMessage("\n", 1);
+//	sendUartMessage("\r", 1);
 	for(;;)
 	{
-		sendUartMessage("\n", 1);
-		sendUartMessage("\r", 1);
-		sendUartMessage("222222", 6);
-		sendUartMessage("\n", 1);
-		sendUartMessage("\r", 1);
-		vTaskDelay(pdMS_TO_TICKS(50));
+//		sendUartMessage("\n", 1);
+//		sendUartMessage("\r", 1);
+//		sendUartMessage("222222", 6);
+//		sendUartMessage("\n", 1);
+//		sendUartMessage("\r", 1);
+		vTaskDelay(pdMS_TO_TICKS(500));
 	}
 }
 
 void test3(void *argument){
-	vTaskDelay(pdMS_TO_TICKS(4000));
+	vTaskDelay(pdMS_TO_TICKS(1000));
 	printAllThreadInfo();
 	sendUartMessage("\n", 1);
 	sendUartMessage("\r", 1);
