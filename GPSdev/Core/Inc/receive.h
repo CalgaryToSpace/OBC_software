9f7e8d41e48a7d8a6b10fc6aef7a4d868724eaad
Matthew Wells <56666479+TheGuy987@users.noreply.github.com>
2022-07-21 18:31:51 -0600

    Added CRC functions included in the OEM7 commands and logs manual

--- GPSdev/Core/Inc/receive.h
+++ GPSdev/Core/Inc/receive.h
@@ -6,6 +6,7 @@
 
 #ifndef INC_RECEIVE_H_
 #define INC_RECEIVE_H_
+#define CRC32_POLYNOMIAL 0xEDB88320L
 
 enum Info{
 	Sync = 0,
@@ -22,6 +23,20 @@ enum Info{
 };
 
 char** gpsParseReceive(char* received);
+
+/* --------------------------------------------------------------------------
+From OEM7_Commands_Logs_Manual.pdf
+Calculate a CRC value to be used by CRC calculation functions.
+-------------------------------------------------------------------------- */
+CRC32Value(int i);
+
+/* --------------------------------------------------------------------------
+From OEM7_Commands_Logs_Manual.pdf
+Calculates the CRC-32 of a block of data all at once
+ulCount - Number of bytes in the data block
+ucBuffer - Data block
+-------------------------------------------------------------------------- */
+CalculateBlockCRC32( unsigned long ulCount, unsigned char*ucBuffer );
 
 #endif /* INC_RECEIVE_H_ */
 
--- GPSdev/Core/Src/receive.c
+++ GPSdev/Core/Src/receive.c
@@ -9,17 +9,17 @@
 #include "receive.h"
 
 char** gpsParseReceive(char* received){
-	if(received[0] != '#'){
+	if(received[0] != '#'){											// Messages should start with "#'
 		return NULL;
 	}
 
-	char** toReturn = (char**) malloc(sizeof(char*) * 11);
+	char** toReturn = (char**) malloc(sizeof(char*) * 11);			// Allocate mem for fields of the response
 	char* current = received;
 	int cursor = 0;
 	int index = 0;
 	int fieldStart = 0;
-	while((*current) != '*'){
-		if((*current) == ',' || (*current) ==';'){
+	while((*current) != '*'){										// "*" indicates the end of the fields
+		if((*current) == ',' || (*current) ==';'){					// Fields are seperated by "," or ";"
 			toReturn[index] = (char*) malloc((sizeof(char) * (cursor - fieldStart)) + 1);
 			int j = 0;
 			for(int i = fieldStart; i < cursor; i++){
@@ -35,4 +35,35 @@ char** gpsParseReceive(char* received){
 			cursor += 1;
 		}
 	}
+	//TODO - implement CRC check
 }
+
+
+unsigned long CRC32Value(int i){
+	int j;
+	unsigned long ulCRC;
+    ulCRC = i;
+    for (j = 8 ; j > 0; j--) {
+    	if ( ulCRC & 1 ){
+    		ulCRC = (ulCRC >> 1) ^ CRC32_POLYNOMIAL;
+    	}
+    	else{
+            ulCRC >>= 1;
+    	}
+    }
+    return ulCRC;
+}
+
+
+unsigned long CalculateBlockCRC32(unsigned long ulCount, unsigned char*ucBuffer){
+	unsigned long ulTemp1;
+	unsigned long ulTemp2;
+	unsigned long ulCRC = 0;
+	while (ulCount-- != 0) {
+		ulTemp1 = (ulCRC >> 8) & 0x00FFFFFFL;
+        ulTemp2 = CRC32Value(((int) ulCRC ^ *ucBuffer++) & 0xFF);
+        ulCRC = ulTemp1 ^ ulTemp2;
+	}
+	return( ulCRC );
+}
+
