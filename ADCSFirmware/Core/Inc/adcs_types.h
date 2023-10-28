513baf929485da1ceb6683e86268399d8f517f96
Saksham Puri <84263133+Saksham-P@users.noreply.github.com>
2023-10-28 11:20:09 -0600

    Updated adcs_types.h to include basic definitions
    Added definitions for Escape character to read Start of Message and End of Message characters

--- ADCSFirmware/Core/Inc/adcs_types.h
+++ ADCSFirmware/Core/Inc/adcs_types.h
@@ -9,14 +9,16 @@
 #define INC_ADCS_TYPES_H_
 
 //Bit 7 of Telecommand and Telemetry Request - Section 4.1 of Firmware Manual
-#define ADCS_Telecommand 0;
-#define ADCS_Telemetery_Request 1;
+#define ADCS_TELECOMMAND 0;
+#define ADCS_TELEMETRY_REQUEST 1;
 
 //ADCS Magic Number defined in Section 5.1 Table 10 of Firmware Manual
 #define ADCS_MAGIC_NUMBER 0x5A;
 
-//Identifiers of Start of Message and End of Message
-#define ADCS_SOM 0x7F;
-#define ADCS_EOM 0xFF;
+//Identifiers of Start of Message and End of Message - Section 4.2 of Firmware Manual
+// 0x1F | 0x7F ....... | 0x1F 0xFF
+#define ADCS_ESC_CHARACTER 0X1F;
+#define ADCS_START_OF_MESSAGE 0x7F;
+#define ADCS_END_OF_MESSAGE 0xFF;
 
 #endif /* INC_ADCS_TYPES_H_ */
