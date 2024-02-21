012f552772236ee4558064ff151eae606e0db208
DeemDeem52 <42725035+DeemDeem52@users.noreply.github.com>
2024-02-20 19:33:53 -0700

    I2C test

--- I2C_Test/Core/Src/main.c
+++ I2C_Test/Core/Src/main.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2023 STMicroelectronics.
+  * Copyright (c) 2024 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -21,8 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "adcs_types.h"
-#include <string.h>
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -41,7 +40,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-I2C_HandleTypeDef hi2c3;
+I2C_HandleTypeDef hi2c1;
 
 UART_HandleTypeDef hlpuart1;
 UART_HandleTypeDef huart3;
@@ -49,9 +48,7 @@ UART_HandleTypeDef huart3;
 PCD_HandleTypeDef hpcd_USB_OTG_FS;
 
 /* USER CODE BEGIN PV */
-
-uint8_t CRC8Table[256];
-
+uint8_t TX_Buffer [] = "A" ; // DATA to send
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -60,21 +57,8 @@ static void MX_GPIO_Init(void);
 static void MX_LPUART1_UART_Init(void);
 static void MX_USART3_UART_Init(void);
 static void MX_USB_OTG_FS_PCD_Init(void);
-static void MX_I2C3_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
-
-// TC/TLM functions
-uint8_t I2C_telecommand_wrapper(uint8_t id, uint8_t* data, uint32_t data_length);
-void send_I2C_telecommand(uint8_t id, uint8_t* data, uint32_t data_length);
-uint8_t send_UART_telecommand(uint8_t id, uint8_t* data, uint32_t data_length);
-
-// CRC functions
-void COMMS_Crc8Init();
-uint8_t COMMS_Crc8Checksum(uint8_t* buffer, uint16_t len);
-
-// UART debug functions
-void PRINT_STRING_UART(void *string);
-void PRINT_NEW_LINE();
 
 /* USER CODE END PFP */
 
@@ -114,55 +98,10 @@ int main(void)
   MX_LPUART1_UART_Init();
   MX_USART3_UART_Init();
   MX_USB_OTG_FS_PCD_Init();
-  MX_I2C3_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
-
-  // Step 1: test I2C connection with Arduino (code from demo)
-
-#define TXBUFFERSIZE (COUNTOF(aTxBuffer) - 1)
-#define COUNTOF(__BUFFER__)   (sizeof(__BUFFER__) / sizeof(*(__BUFFER__)))
-
-  uint8_t aTxBuffer[] = " ****I2C_TwoBoards communication based on Polling****  ****I2C_TwoBoards communication based on Polling****  ****I2C_TwoBoards communication based on Polling**** ";
-  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
-  // turn on blue LED to test transmission
-
-  while(HAL_I2C_Master_Transmit(&hi2c3, (uint8_t)ADCS_I2C_ADDRESS << 1, (uint8_t*)aTxBuffer, TXBUFFERSIZE, 10000)!= HAL_OK)
-	  // trying left-shift of address by 1 due to 7-bit protocol
-  {
-    if (HAL_I2C_GetError(&hi2c3) != HAL_I2C_ERROR_AF)
-    {
-      while (1) {
-    	// repeatedly blink for error
-    	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
-    	HAL_Delay(1000);
-      }
-    }
-  }
-
-  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
-  // turns off on successful transmission
-
-
-
-/*
-  //Testing send_telecommand function
-  //PRINT_STRING_UART("Testing send TC...");
-
-  //Data being transmitted
-  uint8_t data[5] = {10, 11, 12, 13, 14};
-
-  //Length of Data
-  uint32_t data_length = sizeof(data);
-
-  //TC ID (Indicated by 7th bit being 0, TC value < 128)
-  uint8_t id = TC_LOAD_FILE_DOWNLOAD_BLOCK;
-
-  //Calling the send_telecommand function
-  send_I2C_telecommand(id, data, data_length);
-*/
-
-  //TODO: Do something with telemetry reply
-
+  HAL_I2C_Master_Transmit(&hi2c1,0x57 <<1,TX_Buffer,1,1000000000000); //Sending in Blocking mode
+  HAL_Delay(100);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -195,15 +134,14 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-  RCC_OscInitStruct.MSICalibrationValue = 0;
-  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
-  RCC_OscInitStruct.PLL.PLLM = 1;
-  RCC_OscInitStruct.PLL.PLLN = 60;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 2;
+  RCC_OscInitStruct.PLL.PLLN = 30;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
   RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
@@ -218,7 +156,7 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
@@ -228,50 +166,50 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief I2C3 Initialization Function
+  * @brief I2C1 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_I2C3_Init(void)
+static void MX_I2C1_Init(void)
 {
 
-  /* USER CODE BEGIN I2C3_Init 0 */
+  /* USER CODE BEGIN I2C1_Init 0 */
 
-  /* USER CODE END I2C3_Init 0 */
+  /* USER CODE END I2C1_Init 0 */
 
-  /* USER CODE BEGIN I2C3_Init 1 */
+  /* USER CODE BEGIN I2C1_Init 1 */
 
-  /* USER CODE END I2C3_Init 1 */
-  hi2c3.Instance = I2C3;
-  hi2c3.Init.Timing = 0x20600714;
-  hi2c3.Init.OwnAddress1 = 0;
-  hi2c3.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c3.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c3.Init.OwnAddress2 = 0;
-  hi2c3.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-  hi2c3.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c3.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c3) != HAL_OK)
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x107075B0;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Analogue filter
   */
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c3, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Digital filter
   */
-  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c3, 0) != HAL_OK)
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN I2C3_Init 2 */
+  /* USER CODE BEGIN I2C1_Init 2 */
 
-  /* USER CODE END I2C3_Init 2 */
+  /* USER CODE END I2C1_Init 2 */
 
 }
 
@@ -292,7 +230,7 @@ static void MX_LPUART1_UART_Init(void)
   /* USER CODE END LPUART1_Init 1 */
   hlpuart1.Instance = LPUART1;
   hlpuart1.Init.BaudRate = 209700;
-  hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
+  hlpuart1.Init.WordLength = UART_WORDLENGTH_7B;
   hlpuart1.Init.StopBits = UART_STOPBITS_1;
   hlpuart1.Init.Parity = UART_PARITY_NONE;
   hlpuart1.Init.Mode = UART_MODE_TX_RX;
@@ -419,6 +357,7 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOG_CLK_ENABLE();
@@ -462,183 +401,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
-uint8_t I2C_telecommand_wrapper(uint8_t id, uint8_t* data, uint32_t data_length) {
-    // Send telecommand
-    send_I2C_telecommand(id, data, data_length);
-
-    // Poll TC Acknowledge Telemetry Format until the Processed flag equals 1.
-    uint8_t processed = 0;
-    uint8_t tc_ack[4];
-    while (!processed) {
-        send_I2C_telecommand(TLF_TC_ACK, tc_ack, 4);
-        processed = tc_ack[1] & 1;
-    }
-
-    // Confirm telecommand validity by checking the TC Error flag of the last read TC Acknowledge Telemetry Format.
-    send_I2C_telecommand(TLF_TC_ACK, tc_ack, 4);
-    uint8_t TC_err_flag = tc_ack[2];
-
-    return TC_err_flag;
-}
-
-void send_I2C_telecommand(uint8_t id, uint8_t* data, uint32_t data_length) {
-	// Telemetry Request or Telecommand Format:
-	// ADCS_ESC_CHARACTER, ADCS_START_MESSAGE [uint8_t TLM/TC ID], ADCS_ESC_CHARACTER, ADCS_END_MESSAGE
-	// The defines in adcs_types.h already include the 7th bit of the ID to distinguish TLM and TC
-	// data bytes can be up to a maximum of 8 bytes; data_length ranges from 0 to 8
-
-	//Check id to identify if it's Telecommand or Telemetry Request
-	uint8_t telemetry_request = id & 0b10000000; // 1 = TLM, 0 = TC
-
-	//Allocate only required memory by checking first bit of ID
-	uint8_t buf[2 + telemetry_request + (!telemetry_request)*data_length];
-
-	buf[0] = ADCS_I2C_WRITE;
-	buf[1] = id;
-
-	if (telemetry_request) {
-		buf[2] = ADCS_I2C_READ;
-	} else {
-	// Fill buffer with Data if transmitting a Telecommand
-		for (int i = 0; i < data_length; i++) {
-					buf[i + 3] = data[i];
-				}
-	}
-
-	HAL_I2C_Master_Transmit(&hi2c3, ADCS_I2C_ADDRESS, buf, sizeof(buf)/sizeof(uint8_t), HAL_MAX_DELAY);
-
-	if (telemetry_request) {
-		// populate data with received telemetry
-		HAL_I2C_Master_Receive(&hi2c3, ADCS_I2C_ADDRESS, data, data_length, HAL_MAX_DELAY);
-	}
-
-}
-
-uint8_t send_UART_telecommand(uint8_t id, uint8_t* data, uint32_t data_length) {
-	// WARNING: DEPRECATED FUNCTION
-	// This function is incomplete, and will not be updated.
-	// USE AT YOUR OWN RISK.
-
-	// Telemetry Request or Telecommand Format:
-	// ADCS_ESC_CHARACTER, ADCS_START_MESSAGE [uint8_t TLM/TC ID], ADCS_ESC_CHARACTER, ADCS_END_MESSAGE
-	// The defines in adcs_types.h already include the 7th bit of the ID to distinguish TLM and TC
-	// data bytes can be up to a maximum of 8 bytes; data_length ranges from 0 to 8
-
-	//Check id to identify if it's Telecommand or Telemetry Request
-	uint8_t telemetry_request = id & 0b10000000; // 1 = TLM, 0 = TC
-
-	//Allocate only required memory by checking first bit of ID
-	uint8_t buf[5 + (!telemetry_request)*data_length];
-
-	//Fill buffer with ESC, SOM and ID
-	buf[0] = ADCS_ESC_CHARACTER;
-	buf[1] = ADCS_START_MESSAGE;
-	buf[2] = id;
-
-	if (telemetry_request) {
-		//If transmitting Telemetry Request
-		//Fill buffer with ESC and EOM without data_length
-		buf[3] = ADCS_ESC_CHARACTER;
-		buf[4] = ADCS_END_MESSAGE;
-	} else {
-		//Fill buffer with Data if transmitting a Telecommand
-		for (int i = 0; i < data_length; i++) {
-			buf[i + 3] = data[i];
-		}
-		//Fill buffer with ESC and EOM
-		buf[3 + data_length] = ADCS_ESC_CHARACTER;
-		buf[4 + data_length] = ADCS_END_MESSAGE;
-	}
-
-	//Transmit the TLM or TC via UART
-	HAL_UART_Transmit(&hlpuart1, buf, strlen((char*)buf), HAL_MAX_DELAY);
-
-	//receiving from telecommand: data is one byte exactly
-	//receiving from telemetry request: data is up to 8 bytes
-
-	//Allocate only required memory
-	uint8_t buf_rec[6 + (telemetry_request)*(data_length-1)];
-
-	//Start receiving acknowledgment or reply from the CubeComputer
-	HAL_UART_Receive(&hlpuart1, buf_rec, strlen((char*)buf_rec), HAL_MAX_DELAY);
-
-	if (telemetry_request) {
-		//Ignoring ESC, EOM, SOM and storing the rest of the values in data
-		for (int i = 3; i < sizeof(buf_rec)-2; i++) {
-			// put the data into the data array excluding TC ID or TLM ID
-			data[i-3] = buf_rec[i];
-		}
-
-		return TC_ERROR_NONE;
-	}
-
-	return buf_rec[3]; // buf_rec[3] contains the TC Error Flag
-
-  // The reply will contain two data bytes, the last one being the TC Error flag.
-  // The receipt of the acknowledge will indicate that another telecommand may be sent.
-  // Sending another telecommand before the acknowledge will corrupt the telecommand buffer.
-}
-
-
-// CRC initialisation
-// init lookup table for 8-bit crc calculation
-void COMMS_Crc8Init()
-	{
-	int val;
-	for (int i = 0; i < 256; i++)
-	{
-		val = i;
-		for (int j = 0; j < 8; j++)
-		{
-			if (val & 1)
-			val ^= CRC_POLY;
-			val >>= 1;
-		}
-		CRC8Table[i] = val;
-	}
-}
-
-
-
-/***************************************************************************//**
-* Calculates an 8-bit CRC value
-*
-* @param[in] buffer
-* the buffer containing data for which to calculate the crc value
-* @param[in] len
-* the number of bytes of valid data in the buffer
-******************************************************************************/
-uint8_t COMMS_Crc8Checksum(uint8_t* buffer, uint16_t len)
-{
-	if (len == 0) return 0xff;
-
-	uint16_t i;
-	uint8_t crc = 0;
-
-	for (i = 0; i < len; i++)
-		crc = CRC8Table[crc ^ buffer[i]];
-
-	return crc;
-}
-
-
-//Debug function to print a new line (\n) in UART
-void PRINT_NEW_LINE() {
-    char buf[] = "\r\n";
-    HAL_UART_Transmit(&hlpuart1, (uint8_t*) buf, strlen(buf), 100);
-}
-
-//Debug function to print a given string to UART
-void PRINT_STRING_UART(void *string) {
-
-//    char *buff = (char*) string;
-    HAL_UART_Transmit(&hlpuart1, (uint8_t*) string, strlen((char*) string), 100);
-    PRINT_NEW_LINE();
-//    memset(string, 0, strlen((char*) string));
-}
-
-
 
 /* USER CODE END 4 */
 
