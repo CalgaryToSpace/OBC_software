e4452c7633412e4f9456a9b7d7ea2a54f2453a0e
MrBear101 <64572053+MrBear101@users.noreply.github.com>
2024-02-20 19:36:24 -0700

    added methods to correspond to function table, and added Superior Command type

---
+++ OperatingSystem/Core/Inc/function_table.h
@@ -0,0 +1,21 @@
+/*
+ * function_table.h
+ *
+ *  Created on: Feb 20, 2024
+ *      Author: asjad
+ */
+
+#ifndef SRC_FUNCTION_TABLE_H_
+#define SRC_FUNCTION_TABLE_H_
+
+
+// define functions for table
+void system_critical_info();
+void picture();
+void satellite();
+void telemetry();
+void MPI_stuff();
+
+
+
+#endif /* SRC_FUNCTION_TABLE_H_ */
--- OperatingSystem/Core/Src/function_table.c
+++ OperatingSystem/Core/Src/function_table.c
@@ -4,6 +4,22 @@
  *  Created on: Feb 20, 2024
  *      Author: asjad
  */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include "function_table.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+// no idea if this will work
+extern UART_HandleTypeDef hlpuart1;
+
+// define type Superior Command Entry
+typedef struct {
+	uint8_t cmd_byte;
+	void (*cmd_func)();
+} SuperiorCommandEntry;
 
 
 SuperiorCommandEntry command_table[] = {
@@ -21,3 +37,29 @@ SuperiorCommandEntry command_table[] = {
 
 
 };
+
+
+void system_critical_info(){
+	char * buf = "system info task\r\n";
+	HAL_UART_Transmit(&hlpuart1, (uint8_t*)buf , strlen(buf), HAL_MAX_DELAY);
+}
+
+void picture(){
+	char * buf = "picture task\r\n";
+	HAL_UART_Transmit(&hlpuart1, (uint8_t*)buf , strlen(buf), HAL_MAX_DELAY);
+}
+
+void satellite(){
+	char * buf = "satellite metrics task\r\n";
+	HAL_UART_Transmit(&hlpuart1, (uint8_t*)buf , strlen(buf), HAL_MAX_DELAY);
+}
+
+void telemetry(){
+	char * buf = "telemetry task\r\n";
+	HAL_UART_Transmit(&hlpuart1, (uint8_t*)buf , strlen(buf), HAL_MAX_DELAY);
+}
+
+void MPI_Stuff(){
+	char * buf = "MPI_stuff task\r\n";
+	HAL_UART_Transmit(&hlpuart1, (uint8_t*)buf , strlen(buf), HAL_MAX_DELAY);
+}
